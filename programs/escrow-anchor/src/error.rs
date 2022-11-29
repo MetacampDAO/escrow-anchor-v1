d594fe05ddd0235aa096fc46dc4d2e4cb6f5672a
jakeyvee <jakeyveee@gmail.com>
2022-11-30 00:17:27 +0800

    reformate error.rs

--- programs/escrow-anchor/src/error.rs
+++ programs/escrow-anchor/src/error.rs
@@ -1,13 +1,17 @@
 use anchor_lang::prelude::*;
- 
+
 #[error_code]
-pub enum EscrowError{
- #[msg("Token account does not contain sufficient token amount")]
- InsufficientTokenBalance,
-  #[msg("There is a mismatch between given and escrow account's initializer receive token account")]
- InvalidInitializerReceiveTokenAccount,
-  #[msg("There is a mismatch between given and escrow account's initializer")]
- InvalidInitializer,
- #[msg("There is a mismatch between given and escrow account's initializer release token account")]
- InvalidInitializerReleaseTokenAccount,
+pub enum EscrowError {
+    #[msg("Token account does not contain sufficient token amount")]
+    InsufficientTokenBalance,
+    #[msg(
+        "There is a mismatch between given and escrow account's initializer receive token account"
+    )]
+    InvalidInitializerReceiveTokenAccount,
+    #[msg("There is a mismatch between given and escrow account's initializer")]
+    InvalidInitializer,
+    #[msg(
+        "There is a mismatch between given and escrow account's initializer release token account"
+    )]
+    InvalidInitializerReleaseTokenAccount,
 }
